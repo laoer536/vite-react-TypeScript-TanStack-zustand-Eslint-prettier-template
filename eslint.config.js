465ad63ea57b26321c5d376187902b8aa5d51da7
LiuJie <1547698569@qq.com>
2025-09-09 23:18:21 +0800

    build(eslint): add React and React Hooks ESLint plugins
    - Add eslint-plugin-react and eslint-plugin-react-hooks to package.json
    - Configure React settings and rules in eslint.config.js
    - Enable React Hooks rules in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,8 @@ import { defineConfig } from 'eslint/config'
 import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript'
 import { flatConfigs as ImportXFlatConfigs } from 'eslint-plugin-import-x'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig(
@@ -16,7 +18,14 @@ export default defineConfig(
   eslintPluginPrettierRecommended,
   ImportXFlatConfigs.recommended,
   {
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
     settings: {
+      react: {
+        version: 'detect', // 自动检测 React 版本
+      },
       'import-x/resolver-next': [
         createTypeScriptImportResolver({
           alwaysTryTypes: true,
@@ -26,6 +35,8 @@ export default defineConfig(
     },
     rules: {
       'import-x/order': ['error', { alphabetize: { order: 'asc' }, 'newlines-between': 'always' }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 )
